87ce4da81e38a9b7675e5733e9efb94f036fa0a3
Adil Surya <adilsuryasakti@gmail.com>
2026-04-01 13:47:19 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente aspernatur nostrum itaque ipsam aperiam perspiciatis eos deleniti numquam minus nemo molestiae quae hic, rerum nulla eaque iure tempore commodi provident?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
