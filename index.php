e2fc0d4bfca05fa8e5a8c96501cdda003cd7b4df
Adil Surya <adilsuryasakti@gmail.com>
2026-04-01 13:26:39 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba git remote lagi</title>
+</head>
+<body>
+    <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente aspernatur nostrum itaque ipsam aperiam perspiciatis eos deleniti numquam minus nemo molestiae quae hic, rerum nulla eaque iure tempore commodi provident?</p>
+</body>
+</html>
